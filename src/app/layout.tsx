60f8bad45fc521512bb409f3a062de78f58ae7ee
Modewebhost Dailymedia <modewebhostdailymedia@MacBookPro.lan>
2026-09-10 16:00:58 +0100

    feat: embed Facebook JavaScript SDK initialization script in root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
+import Script from 'next/script'
 import './globals.css'
 
 export const metadata: Metadata = {
@@ -13,12 +14,39 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  const fbAppId = process.env.NEXT_PUBLIC_FACEBOOK_APP_ID || process.env.FACEBOOK_APP_ID || ''
+  const fbApiVersion = process.env.NEXT_PUBLIC_FACEBOOK_API_VERSION || 'v19.0'
+
   return (
     <html
       lang="en"
       className={`${GeistSans.variable} ${GeistMono.variable} h-full`}
     >
-      <body className="h-full">{children}</body>
+      <body className="h-full">
+        {children}
+        <Script id="facebook-jssdk-init" strategy="afterInteractive">
+          {`
+            window.fbAsyncInit = function() {
+              FB.init({
+                appId      : '${fbAppId}',
+                cookie     : true,
+                xfbml      : true,
+                version    : '${fbApiVersion}'
+              });
+                
+              FB.AppEvents.logPageView();   
+            };
+
+            (function(d, s, id){
+               var js, fjs = d.getElementsByTagName(s)[0];
+               if (d.getElementById(id)) {return;}
+               js = d.createElement(s); js.id = id;
+               js.src = "https://connect.facebook.net/en_US/sdk.js";
+               fjs.parentNode.insertBefore(js, fjs);
+             }(document, 'script', 'facebook-jssdk'));
+          `}
+        </Script>
+      </body>
     </html>
   )
 }
